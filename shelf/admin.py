fed06431294fede1a285db2209607aeb243231a8
martynaczekala <marticzek@gmail.com>
2015-10-19 14:12:39 +0000

    shelf 1

---
+++ shelf/admin.py
@@ -0,0 +1,8 @@
+from __future__ import unicode_literals, absolute_import
+
+from django.contrib import admin
+
+from .models import Author, Publisher, Book 
+# Register your models here.
+
+admin.site.register([Author, Publisher, Book])
